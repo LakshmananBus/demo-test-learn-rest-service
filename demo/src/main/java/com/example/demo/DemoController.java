1fef17c56ebc964a463a0f9ed3b74ccf210a7773
LakshmananBus <34373241+LakshmananBus@users.noreply.github.com>
2021-02-15 18:46:46 +0530

    changes done

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+	@GetMapping
 	public ResponseEntity<Object> getDemo(){
 		return new ResponseEntity<Object>("Success", HttpStatus.OK);
 	}
