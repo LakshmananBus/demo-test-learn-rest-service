8f72ea706ce9c14ff2759c724777014f9914c810
LakshmananBus <34373241+LakshmananBus@users.noreply.github.com>
2021-02-16 19:30:01 +0530

    changes

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-	@GetMapping(value = "/v1/demo")
+	@GetMapping(value = "/v1/cicd")
 	public ResponseEntity<Object> getDemo(){
 		return new ResponseEntity<Object>("ci/cd integration local success", HttpStatus.OK);
 	}
