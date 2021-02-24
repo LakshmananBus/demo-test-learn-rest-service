e586a1d63525d6963d0557fc3243948887db9241
LakshmananBus <34373241+LakshmananBus@users.noreply.github.com>
2021-02-24 08:46:00 +0530

    Update DemoApplicationTests.java

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -9,5 +9,15 @@ class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+	
+	@Test
+	public void contextLoads1() {
+		System.out.println("------------------------"+"contextLoads1");
+	}
+	
+	@Test
+	public void contextLoads2() {
+		System.out.println("------------------------"+"contextLoads2");
+	}
 
 }
