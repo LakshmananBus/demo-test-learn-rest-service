f58a3c58ef2355796de33bdd7de97f9fd7830e4f
LakshmananBus <34373241+LakshmananBus@users.noreply.github.com>
2022-11-25 18:17:16 +0530

    changes

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,25 +2,28 @@ package com.example.demo;
 
 
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
+
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	}
+		System.out.println("------------------------"+"Testcase 1");
 	
+	}
 	@Test
 	public void contextLoads1() {
-		System.out.println("------------------------"+"contextLoads1");
+		System.out.println("------------------------"+"Testcase 2");
 	}
 	
 	@Test
 	public void contextLoads2() {
-		System.out.println("------------------------"+"contextLoads2");
+		System.out.println("------------------------"+"Testcase 3");
 	}
 
 }
