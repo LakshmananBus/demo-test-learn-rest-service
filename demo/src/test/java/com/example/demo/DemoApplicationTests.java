6b2ae8e29f59f0eb74f54d189e1371db08cfa5e3
LakshmananBus <34373241+LakshmananBus@users.noreply.github.com>
2022-11-28 17:15:45 +0530

    sonar

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,16 +4,26 @@ package com.example.demo;
 
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 
 @SpringBootTest
 class DemoApplicationTests {
+	
+	@Autowired
+	DemoController controller;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws JsonMappingException, JsonProcessingException {
 		System.out.println("------------------------"+"Testcase 1");
+		controller.getMapOfList();
+		//controller.getDemo("", "");
+		//controller.getUsers();
 	
 	}
 	@Test
